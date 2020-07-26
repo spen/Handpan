da0165a394a23dc8c20ad5c742238444b78ee803
sandywyper <sandywyper@gmail.com>
2020-07-26 14:25:34 +0100

    Compact styling changes

--- src/App.js
+++ src/App.js
@@ -40,18 +40,24 @@ const theme = {
     }
   },
   formField: {
+    extend: `
+    flex-direction: row;
+    align-items: center;
+    label {
+      width: 65%;
+    }
+    `,
     border: 'none',
     label: {
       'color': 'white',
       margin: {
         bottom: 'none',
-        top: 'small',
+        top: 'none',
         left: 'small'
       }
     },
   },
   select: {
-    border: '1px solid white',
     background: {
       'color': 'darkBakground'
     },
