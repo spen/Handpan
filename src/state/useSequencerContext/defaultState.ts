36f1a3f53590052cfa0329907c5308efae8a269a
Spen <spen_@hotmail.co.uk>
2020-08-11 11:01:36 +0100

    Lanes are now dynamic, easy mode though - still limited to 10 at the moment

--- src/state/useSequencerContext/defaultState.ts
+++ src/state/useSequencerContext/defaultState.ts
@@ -1,53 +1,76 @@
 export default {
     bpm: 140,
     lanes: [
+        // 1
         {
             muted: false,
             solo: false,
             stepsCount: 16,
             activeSteps: [ 0, 4, 8, 12 ],
         },
+        // 2
         {
             muted: false,
             solo: false,
             stepsCount: 16,
             activeSteps: [ ],
         },
+        // 3
         {
             muted: false,
             solo: false,
             stepsCount: 16,
             activeSteps: [ 2, 5 ],
         },
+        // 4
         {
             muted: false,
             solo: false,
             stepsCount: 16,
             activeSteps: [ 3, 6 ],
         },
+        // 5
         {
             muted: false,
             solo: false,
             stepsCount: 16,
             activeSteps: [ 10 ],
         },
+        // 6 
         {
             muted: false,
             solo: false,
             stepsCount: 16,
             activeSteps: [ 11, 15 ],
         },
+        // 7
         {
             muted: false,
             solo: false,
             stepsCount: 16,
             activeSteps: [ 13 ],
         },
+        // 8
         {
             muted: false,
             solo: false,
             stepsCount: 16,
             activeSteps: [  7, 14 ],
-        }
+        },
+        // 9
+        {
+            muted: false,
+            solo: false,
+            stepsCount: 16,
+            activeSteps: [],
+        },
+        // 10
+        {
+            muted: false,
+            solo: false,
+            stepsCount: 16,
+            activeSteps: [],
+        },
+        // 11
     ]
 }
