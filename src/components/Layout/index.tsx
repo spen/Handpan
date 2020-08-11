a26564fbb8f1bcbf4b1b20da3b92add9703c1ebf
Spen <spen_@hotmail.co.uk>
2020-08-11 14:33:05 +0100

    Lovely gradient BG

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -5,6 +5,16 @@ const LayoutContainer = styled.div`
   display: flex;
   width: 100vw;
   height: 100vh;
+  padding: 20px;
+  box-sizing: border-box;
+
+  /* Permalink - use to edit and share this gradient: https://colorzilla.com/gradient-editor/#371860+0,001d59+100 */
+    background: #371860; /* Old browsers */
+    background: -moz-linear-gradient(-45deg,  #371860 0%, #001d59 100%); /* FF3.6-15 */
+    background: -webkit-linear-gradient(-45deg,  #371860 0%,#001d59 100%); /* Chrome10-25,Safari5.1-6 */
+    background: linear-gradient(135deg,  #371860 0%,#001d59 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#371860', endColorstr='#001d59',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
+
 `;
 
 const Main = styled.div`
